27d804a627fb7544324f4dcd8360bb958f9a9155
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-04-21 20:02:26 +0500

    Error,About,Contact Ep7Part03

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <h2>We're really getting healthy!</h2>
+    </div>
+  );
+};
+
+export default About;
